27c7e93b59d78b2f02e6f360c06b50d8739e8d11
ordin1212 <72280046+ordin1212@users.noreply.github.com>
2022-10-04 13:15:52 +0530

    Update and rename MainActivity to MainActivity.java
     It creates a basic application that gives you only three attempts to login to an application.

--- java-assets/MainActivity.java
+++ java-assets/MainActivity.java
@@ -53,5 +53,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 				startActivity(intent);
 				break;
 		}
+		
+		      b1 = (Button)findViewById(R.id.button);
+      ed1 = (EditText)findViewById(R.id.editText);
+      ed2 = (EditText)findViewById(R.id.editText2);
+
+      b2 = (Button)findViewById(R.id.button2);
+      tx1 = (TextView)findViewById(R.id.textView3);
+      tx1.setVisibility(View.GONE);
+
+      b1.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if(ed1.getText().toString().equals("admin") &&
+               ed2.getText().toString().equals("admin")) {
+                  Toast.makeText(getApplicationContext(),
+                     "Redirecting...",Toast.LENGTH_SHORT).show();
+               }else{
+                  Toast.makeText(getApplicationContext(), "Wrong 
+                     Credentials",Toast.LENGTH_SHORT).show();
+
+                  tx1.setVisibility(View.VISIBLE);
+                  tx1.setBackgroundColor(Color.RED);
+                  counter--;
+                  tx1.setText(Integer.toString(counter));
+
+                  if (counter == 0) {
+                     b1.setEnabled(false);
+                  }
+               }
+         }
+      });
+
+      b2.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            finish();
+         }
 	}
 }
